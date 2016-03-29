1ebc87a5dbfd41e991107ebcb77307718280a58c
Alexandre Bodelot <alexandre.bodelot@gmail.com>
2016-03-29 23:39:24 +0200

    Added user authentication - fix #7

--- src/app/app.js
+++ src/app/app.js
@@ -1,37 +1,52 @@
 var app = angular.module('kpm-ui', ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
-
-  $urlRouterProvider.otherwise('/404');
+app.config(function($stateProvider) {
 
   // Bind routes to controllers
   $stateProvider
-    .state('packages', {
-      url: '/packages',
-      title: 'Package list',
-      controller: 'PackagesController',
-      templateUrl: 'app/modules/packages/packages.html'
-    })
-    .state('package', {
-      url: '/package/:id',
-      title: 'Package detail',
-      controller: 'PackagesController',
-      templateUrl: 'app/modules/packages/details.html'
-    })
-    .state('home', {
-      url: '',
-      onEnter: function($timeout, $state)  {
-        $timeout(function() {
-          $state.go('packages');
-        });
-      }
-    })
-    .state('error404', {
-      templateUrl: 'app/modules/errors/404.html'
-    });
+  .state('home', {
+    url: '',
+    title: 'Welcome to KPM',
+    controller: 'HomeController',
+    templateUrl: 'app/modules/home/home.html'
+  })
+  .state('packages', {
+    url: '/packages',
+    title: 'Package list',
+    controller: 'PackagesController',
+    templateUrl: 'app/modules/packages/packages.html'
+  })
+  .state('package', {
+    url: '/package/:id',
+    title: 'Package detail',
+    controller: 'PackagesController',
+    templateUrl: 'app/modules/packages/details.html'
+  })
+  .state('login', {
+    url: '/login',
+    title: 'Login',
+    controller: 'LoginController',
+    templateUrl: 'app/modules/user/login.html'
+  })
+  .state('signup', {
+    url: '/signup',
+    title: 'Create an account',
+    controller: 'SignupController',
+    templateUrl: 'app/modules/user/signup.html'
+  })
+  .state('error404', {
+    templateUrl: 'app/modules/errors/404.html'
+  });
 });
 
-app.controller('AppController', function($scope) {
-  $scope.config = Config;
-  console.log("AppController()");
+app.controller('AppController', function($rootScope, $sce, Session) {
+  // Expose modules in root scope for templates convenience
+  $rootScope.config = Config;
+  $rootScope.session = Session;
+
+  // Update page title on state change
+  $rootScope.$on('$stateChangeSuccess', function(event, toState) {
+    $rootScope.pageTitle = $sce.trustAsHtml('KPM | ' + toState.title);
+  });
 });
+
