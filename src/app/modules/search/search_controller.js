51038a5a3df104756648c290c916e1b42c3f6d45
Alexandre Bodelot <alexandre.bodelot@gmail.com>
2016-03-30 23:11:47 +0200

    Added angular-material, search packages with autocomplete - fix #11

--- src/app/modules/search/search_controller.js
+++ src/app/modules/search/search_controller.js
@@ -1,3 +1,19 @@
-app.controller('SearchController', function($scope, KpmApi) {
+app.controller('SearchController', function($scope, $state, $q, KpmApi) {
 
+  var self = this;
+
+  this.querySearch = function(search) {
+    var deferred = $q.defer();
+    KpmApi.get('packages', {
+      named_like: search
+    })
+    .success(function(data) {
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  };
+
+  this.itemSelected = function(item) {
+    $state.go('package', {name: item.name});
+  };
 });
