68d9eb40b6f40c6efe2803edec24de49e82461a0
Antoine Legrand <antoine.legrand@arkena.com>
2016-04-18 15:08:24 +0200

    Bump version: 0.12.1 → 0.13.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ test_requirements = [
 
 setup(
     name='kpm',
-    version='0.12.1',
+    version='0.13.0',
     description="KPM cli",
     long_description=readme + '\n\n' + history,
     author="Antoine Legrand",
