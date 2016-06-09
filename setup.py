b6d0704aca909587a73f9cf82092d119305fa89b
Antoine Legrand <2t.antoine@gmail.com>
2016-06-09 14:43:25 +0200

    Bump version: 0.16.1 → 0.16.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ test_requirements = [
 
 setup(
     name='kpm',
-    version='0.16.1',
+    version='0.16.2',
     description="KPM cli",
     long_description=readme + '\n\n' + history,
     author="Antoine Legrand",
