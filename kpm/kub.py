3af65ffdc8fb0c3ae62d27b6fadbbf0558580abf
Antoine Legrand <2t.antoine@gmail.com>
2016-06-09 14:36:53 +0200

    Remove ordered dict

--- kpm/kub.py
+++ kpm/kub.py
@@ -27,17 +27,6 @@ logger = logging.getLogger(__name__)
 _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
 
 
-def dict_representer(dumper, data):
-    return dumper.represent_dict(data.iteritems())
-
-
-def dict_constructor(loader, node):
-    return OrderedDict(loader.construct_pairs(node))
-
-
-yaml.add_representer(OrderedDict, dict_representer)
-yaml.add_constructor(_mapping_tag, dict_constructor)
-
 jinja_env = jinja2.Environment()
 jinja_env.filters.update(jinja_filters.filters())
 
