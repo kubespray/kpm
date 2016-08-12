90e855a9f7fe8120c028487a2547331ae57c5b0b
Alexandre Bodelot <alexandre.bodelot@gmail.com>
2016-08-12 19:20:16 +0000

    Added signup form - fix #8

--- src/app/modules/user/signup_controller.js
+++ src/app/modules/user/signup_controller.js
@@ -1,2 +1,35 @@
-app.controller('SignupController', function($scope) {
+app.controller('SignupController', function($scope, $state, KpmApi, Session) {
+
+  // Create user
+  $scope.submit = function() {
+    KpmApi.post('users', {
+      user: {
+        'username': $scope.username,
+        'email': $scope.email,
+        'password': $scope.password,
+        'password_confirmation': $scope.password_confirmation
+      }
+    })
+    .success(function(data) {
+      // Login user after sign-up
+      Session.username = data.username;
+      Session.email = data.email;
+      KpmApi.authorization_token = data.token;
+
+      // Redirect to homepage
+      $state.go('packages');
+    })
+    .error(function(data) {
+      if (data.errors) {
+        $scope.errors = [];
+        for (key in data.errors) {
+          // Build error message from API error reporting
+          $scope.errors.push(key + ' ' + data.errors[key].join(' '));
+        }
+      }
+      else {
+        $scope.errors = ['Oh no, something wrong happened!'];
+      }
+    });
+  };
 });
