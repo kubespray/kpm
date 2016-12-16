ecbb4ca8f819bfb9f9f173cf9a4bcbf290c096df
Antoine Legrand <antoine.legrand@coreos.com>
2016-12-16 17:54:39 +0100

    Update server-side build path

--- kpm/api/builder.py
+++ kpm/api/builder.py
@@ -1,14 +1,14 @@
 from flask import jsonify, Blueprint, current_app
 import kpm.api.impl.builder
 from kpm.api.app import getvalues
+from cnr.api.app import repo_name
 
 
 builder_app = Blueprint('builder', __name__,)
 
 
-def _build(package):
+def _build(package, version, media_type='kpm'):
     values = getvalues()
-    version = values.get('version', None)
     namespace = values.get('namespace', None)
     variables = values.get('variables', {})
     shards = values.get('shards', None)
@@ -22,29 +22,39 @@ def _build(package):
     return k
 
 
-@builder_app.route("/api/v1/packages/<path:package>/file/<path:filepath>")
-def show_file(package, filepath):
-    return kpm.api.impl.builder.show_file(package, filepath,
+@builder_app.route("/api/v1/packages/<string:namespace>/<string:package_name>/file/<path:filepath>")
+def show_file(namespace, package_name, filepath):
+    reponame = repo_name(namespace, package_name)
+
+    return kpm.api.impl.builder.show_file(reponame, filepath,
                                           endpoint=current_app.config['KPM_REGISTRY_HOST'])
 
 
-@builder_app.route("/api/v1/packages/<path:package>/tree")
-def tree(package):
-    r = kpm.api.impl.builder.tree(package,
+@builder_app.route(
+    "/api/v1/packages/<string:namespace>/<string:package_name>/<string:release>/<string:media_type>/tree")
+def tree(namespace, package_name, release, media_type):
+    reponame = repo_name(namespace, package_name)
+    r = kpm.api.impl.builder.tree(reponame,
                                   endpoint=current_app.config['KPM_REGISTRY_HOST'])
     return jsonify(r)
 
 
-@builder_app.route("/api/v1/packages/<path:package>/generate", methods=['POST', 'GET'])
-def build(package):
-    current_app.logger.info("generate %s", package)
-    k = _build(package)
+@builder_app.route(
+    "/api/v1/packages/<string:namespace>/<string:package_name>/<string:release>/<string:media_type>/generate",
+    methods=['POST', 'GET'])
+def build(namespace, package_name, release, media_type):
+    reponame = repo_name(namespace, package_name)
+    current_app.logger.info("generate %s", namespace, package_name)
+    k = _build(reponame, release, media_type)
     return jsonify(k.build())
 
 
-@builder_app.route("/api/v1/packages/<path:package>/generate-tar", methods=['POST', 'GET'])
-def build_tar(package):
-    k = _build(package)
+@builder_app.route(
+    "/api/v1/packages/<string:namespace>/<string:package_name>/<string:release>/<string:media_type>/generate-tar",
+    methods=['POST', 'GET'])
+def build_tar(namespace, package_name, release, media_type):
+    reponame = repo_name(namespace, package_name)
+    k = _build(reponame, release, media_type)
     resp = current_app.make_response(k.build_tar())
     resp.mimetype = 'application/tar'
     resp.headers['Content-Disposition'] = 'filename="%s_%s.tar.gz"' % (k.name.replace("/", "_"), k.version)
