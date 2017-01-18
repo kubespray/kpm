b33fcb7cd90a46b9efb7fd9bc03366347377d4da
Antoine Legrand <antoine.legrand@coreos.com>
2017-01-18 17:46:55 +0100

    Add run-server.sh with options

--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-PORT=$1
-gunicorn kpm.api.wsgi:app -b :$PORT --timeout 120 -w 4 --reload
+PORT=${PORT:-5000}
+storage="${STORAGE:-filesystem}"
+echo $storage
+db_class=cnr.models.kv.$storage.db:CnrDB
+blob_class=cnr.models.kv.$storage.blob:Blob
+echo $db_class
+DATABASE_URL="$HOME/.kpm/packages" CNR_DB_CLASS=$db_class CNR_BLOB_CLASS=$blob_class gunicorn kpm.api.wsgi:app -b :$PORT --timeout 120 -w 4 --reload
